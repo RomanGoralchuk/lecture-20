80287e82290f2d5abfa7949dc8d0beb4e1b895b6
Roman Goralchuk <gromka.vladyko@gmail.com>
2021-12-09 15:55:59 +0300

    Spring hierarchy control

--- hospital-persistence/src/main/java/by/itacademy/javaenterprise/goralchuk/dao/impl/PatientDaoImpl.java
+++ hospital-persistence/src/main/java/by/itacademy/javaenterprise/goralchuk/dao/impl/PatientDaoImpl.java
@@ -5,7 +5,6 @@ import by.itacademy.javaenterprise.goralchuk.entity.client.LifeStatus;
 import by.itacademy.javaenterprise.goralchuk.entity.client.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
